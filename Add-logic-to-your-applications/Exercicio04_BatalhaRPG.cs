8ecd4ad999c4f7b31f80137bb5a7f9c1d7c709a5
DiegoSkieresz <46655332+SkiereszDiego@users.noreply.github.com>
2022-07-06 17:31:09 -0300

    Create Exercicio04_BatalhaRPG.cs

---
+++ Add-logic-to-your-applications/Exercicio04_BatalhaRPG.cs
@@ -0,0 +1,17 @@
+//  Um herói e um monstro começam com a mesma pontuação de integridade. Durante a rodada do herói,
+//  ele gerará um valor aleatório que será subtraído da integridade do monstro. Se a integridade
+//  do monstro for maior que zero, ela terá sua rodada e atacará o herói. Desde que tanto o herói
+//  quanto o monstro tenham integridade maior que zero,a batalha continuará.
+// Regras do Jogo
+
+// O herói e o monstro começarão com dez pontos de integridade.
+// Todos os ataques serão um valor entre 1 e 10.
+// O herói atacará primeiro.
+// Imprima a quantidade de integridade que o monstro perdeu e a integridade que resta a ele.
+// Se a integridade do monstro for maior que zero, ele poderá atacar o herói.
+// Imprima a quantidade de integridade que o herói perdeu e a integridade que resta a ele.
+// Continue esta sequência de ataque até que a integridade do monstro ou do herói seja zero ou menos.
+// Imprima quem foi o vencedor.
+// O mais importante é:
+
+// Você precisa usar a instrução do-while ou a instrução while.
