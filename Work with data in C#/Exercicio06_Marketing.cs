89bf1b3a1f279217f6de83cd4152dffd152174d4
DiegoSkieresz <46655332+SkiereszDiego@users.noreply.github.com>
2022-07-11 11:30:26 -0300

    Challenge_Marketing

---
+++ Work with data in C#/Exercicio06_Marketing.cs
@@ -0,0 +1,53 @@
+// Use o Editor do .NET para gerar a mensagem, considerando as variáveis e o
+// código. Substitua os comentários do código pelo seu próprio código de
+// formatação de cadeia de caracteres.
+// string customerName = "Mr. Jones";
+
+// string currentProduct = "Magic Yield";
+// int currentShares = 2975000;
+// decimal currentReturn = 0.1275m;
+// decimal currentProfit = 55000000.0m;
+
+// string newProduct = "Glorious Future";
+// decimal newReturn = 0.13125m;
+// decimal newProfit = 63000000.0m;
+
+// // Your logic here
+
+// Console.WriteLine("Here's a quick comparison:\n");
+
+// string comparisonMessage = "";
+
+// // Your logic here
+
+// Console.WriteLine(comparisonMessage);
+string customerName = "Mr. Jones";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+Console.WriteLine(comparisonMessage);
